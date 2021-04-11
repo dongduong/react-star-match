8f900ff6bd7f2842353dbf1b2df124ad1cd8b5be
Dong Duong <dhdong1510@gmail.com>
2021-04-11 22:59:39 +0700

    view function - from state to UI

--- src/example/star-match/star-match.js
+++ src/example/star-match/star-match.js
@@ -10,13 +10,33 @@ const StarDisplay = props => (
 )
 
 const PlayNumber = props => (
-    <button className="number" onClick={() => console.log('Num', props.number)}>
+    <button 
+        className="number"
+        style={{ backgroundColor: colors[props.status] }}
+        onClick={() => console.log('Num', props.number)}
+    >
         {props.number}
     </button>
 )
 
 const StarMatch = () => {
     const [stars, setStars] = useState(utils.random(1, 9));
+    const [availableNums, setAvailableNums] = useState([1,2,3,4,5]);
+    const [candidateNums, setCandidateNums] = useState([2,3]);
+
+    const candidatesAreWrong = utils.sum(candidateNums) > stars;
+
+    const numberStatus = (number) => {
+        if (!availableNums.includes(number)) {
+            return 'used';
+        }
+
+        if (candidateNums.includes(number)) {
+            return candidatesAreWrong ? 'wrong' : 'candidate';
+        }
+
+        return 'available'
+    }
     return (
       <div className="game">
         <div className="help">
@@ -28,7 +48,11 @@ const StarMatch = () => {
           </div>
           <div className="right">
             {utils.range(1, 9).map(number =>
-                <PlayNumber key={number} number={number}/>
+                <PlayNumber
+                    key={number}
+                    status={numberStatus(number)}
+                    number={number}
+                />
             )}
           </div>
         </div>
