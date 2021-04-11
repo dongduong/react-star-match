7e352db9dcc589eb98f35bcbdc849ac6f93ae73b
Dong Duong <dhdong1510@gmail.com>
2021-04-12 00:25:05 +0700

    Behavior functions

--- src/example/star-match/star-match.js
+++ src/example/star-match/star-match.js
@@ -13,7 +13,7 @@ const PlayNumber = props => (
     <button 
         className="number"
         style={{ backgroundColor: colors[props.status] }}
-        onClick={() => console.log('Num', props.number)}
+        onClick={() => props.onClick(props.number, props.status)}
     >
         {props.number}
     </button>
@@ -21,8 +21,8 @@ const PlayNumber = props => (
 
 const StarMatch = () => {
     const [stars, setStars] = useState(utils.random(1, 9));
-    const [availableNums, setAvailableNums] = useState([1,2,3,4,5]);
-    const [candidateNums, setCandidateNums] = useState([2,3]);
+    const [availableNums, setAvailableNums] = useState(utils.range(1, 9));
+    const [candidateNums, setCandidateNums] = useState([]);
 
     const candidatesAreWrong = utils.sum(candidateNums) > stars;
 
@@ -37,6 +37,31 @@ const StarMatch = () => {
 
         return 'available'
     }
+
+    const onNumberClick = (number, currentStatus) => {
+        if (currentStatus == 'used'){
+            return;
+        }
+
+        const newCandidateNums = 
+            currentStatus === 'available'
+                ? candidateNums.concat(number)
+                : candidateNums.filter(cn => cn !== number);
+        
+        if(utils.sum(newCandidateNums) !== stars) {
+            setCandidateNums(newCandidateNums);
+        } else {
+            const newAvailableNums = availableNums.filter(
+                n => !newCandidateNums.includes(n)
+            );
+
+            //redraw stars (from what's available)
+            setStars(utils.randomSumIn(newAvailableNums, 9));
+            setAvailableNums(newAvailableNums);
+            setCandidateNums([]);
+        }
+    }
+
     return (
       <div className="game">
         <div className="help">
@@ -52,6 +77,7 @@ const StarMatch = () => {
                     key={number}
                     status={numberStatus(number)}
                     number={number}
+                    onClick={onNumberClick}
                 />
             )}
           </div>
